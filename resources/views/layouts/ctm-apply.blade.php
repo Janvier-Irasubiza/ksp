7e9bb5d1a151885669b95c8d126ee098b6f6cf51
Janvier-Irasubiza <irasubizajanvier99@gmail.com>
2024-06-26 21:21:41 +0200

    Logo

--- resources/views/layouts/ctm-apply.blade.php
+++ resources/views/layouts/ctm-apply.blade.php
@@ -21,11 +21,11 @@
     <body class="font-sans text-gray-900 antialiased">            
         <div class="min-h-screen w-100 flex mt-6 mb-6 flex-col sm:justify-center items-center sm:pt-0 bg-gray-100">
         <div class="flex flex-col sm:justify-center items-center">
-            <div>
+            <!-- <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
-            </div>
+            </div> -->
 
             <div class="w-50 mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
