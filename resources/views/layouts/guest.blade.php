8e8af5495b70b829dd3111e7937158da7fc0aa64
Janvier-Irasubiza <irasubizajanvier99@gmail.com>
2024-06-28 14:10:23 +0200

    Logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <img src="{{ asset('images/logo_ksp.svg) }}" alt="Logo">
+                    <img src="{{ asset('images/logo_ksp.svg') }}" alt="Logo">
                 </a>
             </div>
 
